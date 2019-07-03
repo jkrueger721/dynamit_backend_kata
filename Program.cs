19cef9a256a6bb9959b2983a833d762cb2e0f453
Johnathan Krueger <johnathankrueger@LAPTOP-LR6RV2GM.attlocal.net>
2019-07-03 15:29:12 -0400

    add file Output.txt to display output

--- Program.cs
+++ Program.cs
@@ -9,11 +9,7 @@ class Program
 
     static void Main(string[] args)
     {
-        // int i;
-        // int count;
-        // int item;
-
-
+    
         var text = File.ReadAllText(@"Paragraph.txt");
         var punctuation = text.Where(Char.IsPunctuation).Distinct().ToArray();
         var words = text.Split().Select(x => x.Trim(punctuation)).ToArray();
@@ -30,13 +26,16 @@ class Program
                     dictionary.Add(word,1);
                 }
             }
-        // Dictionary<string, int>.ValueCollection vals = dictionary.Values;
+        // Test values of key pairing
         Console.WriteLine();
         foreach( KeyValuePair<string, int> kvp in dictionary )
         {
             Console.WriteLine("Key = {0}, Value = {1}", 
                 kvp.Key, kvp.Value);
         }
+        using (StreamWriter file = new StreamWriter("Output.txt"))
+        foreach (var entry in dictionary)
+        file.WriteLine("This word: {0}, Occurs {1} times", entry.Key, entry.Value); 
         
     }
 }
