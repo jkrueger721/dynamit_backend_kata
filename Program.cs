5661a9b66e3e010e3368d9d02b9fc110f22760db
Johnathan Krueger <johnathankrueger@LAPTOP-LR6RV2GM.attlocal.net>
2019-07-03 15:10:29 -0400

    read file and sort into dict to count

--- Program.cs
+++ Program.cs
@@ -9,13 +9,34 @@ class Program
 
     static void Main(string[] args)
     {
+        // int i;
+        // int count;
+        // int item;
+
 
         var text = File.ReadAllText(@"Paragraph.txt");
         var punctuation = text.Where(Char.IsPunctuation).Distinct().ToArray();
         var words = text.Split().Select(x => x.Trim(punctuation)).ToArray();
-        Console.WriteLine(words[2]);
-        foreach( string w in words){
-            Console.WriteLine(w);
+
+          Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                if (dictionary.ContainsKey(word))
+                {
+                    dictionary[word] += 1; 
+                }
+                else
+                {
+                    dictionary.Add(word,1);
+                }
+            }
+        // Dictionary<string, int>.ValueCollection vals = dictionary.Values;
+        Console.WriteLine();
+        foreach( KeyValuePair<string, int> kvp in dictionary )
+        {
+            Console.WriteLine("Key = {0}, Value = {1}", 
+                kvp.Key, kvp.Value);
         }
+        
     }
 }
